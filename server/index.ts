8a8927b43339431583f2686264acf21ab568c905
Aditya Gupta <aditya@Adityas-MacBook-Air.local>
2025-11-05 16:01:31 +0530

    First version – ready for deploy

--- server/index.ts
+++ server/index.ts
@@ -70,12 +70,18 @@ app.use((req, res, next) => {
   // Other ports are firewalled. Default to 5000 if not specified.
   // this serves both the API and the client.
   // It is the only port that is not firewalled.
-  const port = parseInt(process.env.PORT || '5000', 10);
-  server.listen({
-    port,
-    host: "0.0.0.0",
-    reusePort: true,
-  }, () => {
-    log(`serving on port ${port}`);
-  });
+  // const port = parseInt(process.env.PORT || '1999', 10);
+  // server.listen({
+  //   port,
+  //   // host: "0.0.0.0",
+  //   reusePort: true,
+  // }, () => {
+  //   log(`serving on port ${port}`);
+  // });
+  const port = Number(process.env.PORT) || 1999;
+
+server.listen(port, () => {
+  console.log(`🚀 QUIZ LIVE → http://localhost:${port}`);
+  console.log(`Press Ctrl+C to stop`);
+});
 })();
